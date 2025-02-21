95fcb86a37e248212c6cb4c5f645ea427a7c3b64
Chatlada-Nina <chatladanine@gmail.com>
2025-02-21 09:47:56 +0100

    Create base template

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 
 def homepage(request):
-    return HttpResponse("This is Homepage!")
+    """
+    A view to return the index page.
+    """
+    return render(request, 'home/index.html')
