3e7477acac9b02e2e8b77d0361054be70dd385af
Chatlada-Nina <chatladanine@gmail.com>
2025-02-25 11:39:38 +0100

    Add menu list filter

--- menu/views.py
+++ menu/views.py
@@ -9,9 +9,18 @@ def all_menus(request):
     A view to show all menu, including sorting.
     """
     menus = MenuItem.objects.all()
+    menu_lists = []
+
+    if request.GET:
+        if 'menu_list' in request.GET:
+            menu_lists = request.GET['menu_list'].split(',')
+            menus = menus.filter(menu_list__name__in=menu_lists)
+            menu_lists = MenuList.objects.filter(name__in=menu_lists)
+
 
     context = {
         'menus': menus,
+        'current_menu_lists': menu_lists,
     }
 
     return render(request, 'menu/menus.html', context)
