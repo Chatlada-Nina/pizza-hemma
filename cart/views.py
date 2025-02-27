73260e9b43d41e23baa7082ba499a3d6af0c28db
Chatlada-Nina <chatladanine@gmail.com>
2025-02-27 15:45:00 +0100

    Add update and remove item on the cart page

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -14,8 +14,8 @@ def add_to_cart(request, menu_id):
     """
     Add a quantity of the specified menu to the cart.
     """
-    quantity = int(request.GET.get('quantity'))
-    redirect_url = request.GET.get('redirect_url')
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if menu_id in list(cart.keys()):
@@ -25,3 +25,38 @@ def add_to_cart(request, menu_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def update_cart(request, menu_id):
+    """
+    Update the quantity of the specified menu to the specified amount.
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[menu_id] = quantity
+    else:
+        cart.pop(menu_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+
+def remove_from_cart(request, menu_id):
+    """
+    Remove the menu from the cart.
+    """
+    try:
+        cart = request.session.get('cart', {})
+
+        if menu_id in cart:
+            cart.pop(menu_id)
+
+        request.session['cart'] = cart
+        print(cart)
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        return HttpResponse(status=500)
