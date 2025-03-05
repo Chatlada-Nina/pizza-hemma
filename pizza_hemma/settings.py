a90eed52136784665d1d33d3c8bda31335b8f829
Chatlada-Nina <chatladanine@gmail.com>
2025-03-05 14:58:35 +0100

    Edit static files name

--- pizza_hemma/settings.py
+++ pizza_hemma/settings.py
@@ -183,8 +183,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_pizza'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_pizza')
 
 FREE_DELIVERY_THRESHOLD = 800
 STANDARD_DELIVERY_PERCENTAGE = 20
