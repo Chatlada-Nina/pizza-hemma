96370c052f000496b5ff754ad95f805bac88eb1d
Chatlada-Nina <chatladanine@gmail.com>
2025-03-26 19:46:57 +0100

    Fix the populated name and email fields on the contact form

--- contact/views.py
+++ contact/views.py
@@ -15,19 +15,32 @@ def contact_us(request):
     :template:`contact/contactus.html`
     """
     if request.method == "POST":
+
         if request.user.is_authenticated:
-            contact_form = ContactForm(data=request.POST, initial={
-                'name': request.user.get_full_name(),
-                'email': request.user.email,
-            })
+
+            user_name = request.user.get_full_name() or request.user.username
+            contact_form = ContactForm(data=request.POST)
+            contact_form.initial = {
+                'name': user_name, 'email': request.user.email}
+
+            if contact_form.is_valid():
+                contact = contact_form.save(commit=False)
+                contact.name = user_name
+                contact.email = request.user.email
+                contact.save()
+
+                messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    "Your request has been received. "
+                    "We aim to reply within 2 business days."
+                )
+                contact_form = ContactForm()
         else:
             contact_form = ContactForm(data=request.POST)
 
         if contact_form.is_valid():
             contact = contact_form.save(commit=False)
-            if request.user.is_authenticated:
-                contact.name = request.user.get_full_name()
-                contact.email = request.user.email
             contact.save()
 
             messages.add_message(
@@ -41,8 +54,9 @@ def contact_us(request):
     else:
         initial_data = {}
         if request.user.is_authenticated:
+            user_name = request.user.get_full_name() or request.user.username
             initial_data = {
-                'name': request.user.get_full_name(),
+                'name': user_name,
                 'email': request.user.email,
             }
         contact_form = ContactForm(initial=initial_data)
